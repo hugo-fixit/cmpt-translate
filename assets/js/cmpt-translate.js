18db42c484d2f6330fa7317d467c7f870f425be4
Cell <1024@lruihao.cn>
2024-12-03 12:57:15 +0800

    Feat: 完成桌面端自动翻译选择逻辑

--- assets/js/cmpt-translate.js
+++ assets/js/cmpt-translate.js
@@ -10,29 +10,71 @@ class AutoTranslate {
       'post-author',
       'powered',
       'author',
+      'typeit',
       ...params.ignoreClass,
     ];
   }
 
-  addClickLangEvent() {
-    const langSwitch = document.querySelector('.language-switch.auto');
-    // TODO: add click event to switch language and add query param 'lang' to url
+  bindEvents() {
+    // Desktop
+    const switchDesktop = document.querySelector('#header-desktop .language-switch.auto');
+    const switchMenuDesktop = switchDesktop.querySelector('.sub-menu');
+    // Artificial language items by Hugo project
+    const artificialItems = Array.from(switchMenuDesktop.childNodes).filter((node) => node.dataset.type === 'artificial');
+    fixit.util.forEach(artificialItems, (item) => {
+      item.addEventListener('click', (e) => {
+       translate.language.clearCacheLanguage()
+      });
+    })
+    // Machine language items by translate.js service
+    const machineItems = Array.from(switchMenuDesktop.childNodes).filter((node) => node.dataset.type === 'machine');
+    fixit.util.forEach(machineItems, (item) => {
+      item.addEventListener('click', (e) => {
+        // set query param 'lang' to url
+        const lang = item.children[0].dataset.lang;
+        window.history.pushState({}, '', `?lang=${lang}`);
+
+        // toggle active class
+        machineItems.forEach((item) => {
+          item.classList.remove('active');
+        });
+        item.classList.add('active');
+
+        // translate to selected language
+        translate.changeLanguage(lang);
+      });
+    })
+    switchDesktop.previousElementSibling.classList.add('d-none');
+    switchDesktop.classList.remove('d-none');
+
+    // Mobile
+    // TODO
   }
 
-  setupTranslate() {
+  setup() {
+    if (params.detectionLocalLanguage) {
+      translate.setAutoDiscriminateLocalLanguage();
+    }
+    // Set active class for current language (only machine translation)
+    const current = translate.language.getCurrent();
+    const local = translate.language.getLocal();
+    const lang = window.location.search.split('lang=')[1];
+    if (current === local || lang) {
+      fixit.util.forEach(document.querySelectorAll(`.menu-link[data-lang="${lang}"]`), (link) => {
+        link.parentElement.classList.add('active');
+      });
+    }
     translate.ignore.class.push(...this.#IGNORE_CLASS);
-    translate.setAutoDiscriminateLocalLanguage();
     translate.service.use('client.edge');
     translate.language.setUrlParamControl('lang');
     translate.listener.start();
     translate.selectLanguageTag.show = false;
-    console.log(params.languages);
     translate.execute();
   }
 
   init() {
-    this.addClickLangEvent();
-    this.setupTranslate();
+    this.bindEvents();
+    this.setup();
   }
 }
 
